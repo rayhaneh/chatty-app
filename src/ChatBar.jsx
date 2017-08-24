340c1c067b5ccc63324299f38c0f200fb81ceefd
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-24 14:29:41 +0000

    Changed the listener on the username field to onSubmit (project requirement).

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,8 +4,9 @@ class ChatBar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input onChange={this.props.getNewUser}
-        className="chatbar-username" placeholder="Your Name (Optional)" name="username"/>
+        <form onSubmit={this.props.getNewUser}>
+          <input className="chatbar-username" placeholder="Your Name (Optional)" name="username"/>
+        </form>
         <form onSubmit={this.props.sendNewMessage}>
           <input className="chatbar-message" placeholder="Type a message and hit ENTER" name="text" />
         </form>
