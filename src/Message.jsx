e7c006b1768ef615a1e42bea971c6c0162f71d93
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-24 15:59:44 +0000

    Rendering the message content based on exsitence of an image link. The image link is found using regular expression.

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,15 +2,35 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    if (this.props.message.type === 'postMessage') {
+    let re = /(?:(?:https?|ftp):\/\/)?(?:w{3}\.)?\S+\.\S+\/.+\.(?:png|jpg|gif)/
 
-      return (
-        <div className="message">
-          <span className="message-username">{this.props.message.color}</span>
-          <span className="message-username" style={{color: 'red'}}>{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
-        </div>
-      )
+    if (this.props.message.type === 'postMessage') {
+      let image = this.props.message.content.match(re)
+      if (!image) {
+
+        return (
+          <div className="message">
+            <span className="message-username">{this.props.message.color}</span>
+            <span className="message-username" style={{color: 'red'}}>{this.props.message.username}</span>
+            <span className="message-content">{this.props.message.content}</span>
+          </div>
+        )
+      }
+      else {
+        let content = this.props.message.content.split(image[0])
+        return (
+          <div className="message">
+            <span className="message-username">{this.props.message.color}</span>
+            <span className="message-username" style={{color: 'red'}}>{this.props.message.username}</span>
+            <div className="message-content">
+              <span>{content[0]}</span>
+              <div className="message-image"><img src={image[0]} /></div>
+              <span>{content[1]}</span>
+            </div>
+          </div>
+        )
+      }
+
     }
     else {
       return (
