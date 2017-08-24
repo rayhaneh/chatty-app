cc06cb01faff2269e2db5cd95447e8913e65c287
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-24 22:30:10 +0000

    Added an icon to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
+        <a href="/" className="navbar-brand"><i className="fa fa-lg fa-comments-o" aria-hidden="true"></i>  Chatty</a>
         <div>{this.props.onlineUserCounter} user(s) online</div>
       </nav>
     );
