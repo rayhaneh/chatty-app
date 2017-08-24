0bc032bb78c2ff4b459b5f0687533f3bec75f3ad
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-24 14:30:03 +0000

    Added the online user counter to the navbar.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,6 +5,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
+        <div>{this.props.onlineUserCounter} user(s) online</div>
       </nav>
     );
   }
